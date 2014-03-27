21c3033f96f0b084cbf86ad39b44ca3929857bf7
Mac <mac@veryhappythings.co.uk>
2014-03-27 10:52:30 +0000

    Another example fetching all player IDs from recent matches

---
+++ examples/players_from_all_matches.py
@@ -0,0 +1,16 @@
+"""
+This example fetches the player IDs from all the matches that an individual
+player has played in.
+"""
+from dotamatch import get_key
+from dotamatch.history import MatchHistory
+
+key = get_key()
+history = MatchHistory(key)
+
+players = set()
+for match in history.matches(account_id=507891):
+    for player in match.players:
+        players.add(player['account_id'])
+
+print players
