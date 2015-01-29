ac8ee5e3f68f89790335f1d6be833281d2a64d30
Mac Chapman <mac@veryhappythings.co.uk>
2015-01-29 14:17:27 +0000

    Sketching a single-object API approach

--- dotamatch/__init__.py
+++ dotamatch/__init__.py
@@ -13,3 +13,26 @@ def get_key():
             # Python 3
             key = input("Enter Steam API key: ")
     return key
+
+
+class Dota(object):
+    def __init__(self, key):
+        self.key = key
+        self.economy = Economy(key)
+        self.heroes = Heroes(key)
+        self.match_history = MatchHistory(key)
+        self.match_history_by_sequence_num = MatchHistoryBySequenceNum(key)
+        self.league_listing = LeagueListing(key)
+        self.match_details = MatchDetails(key)
+        self.player_summaries = PlayerSummaries(key)
+        self.resolve_vanity_url = ResolveVanityUrl(key)
+        self.teams = Teams(key)
+
+
+from .economy import Economy
+from .heroes import Heroes
+from .history import MatchHistory, MatchHistoryBySequenceNum
+from .leagues import LeagueListing
+from .matches import MatchDetails
+from .players import PlayerSummaries, ResolveVanityUrl
+from .teams import Teams
